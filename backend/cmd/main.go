dc6b1a1e7b31ddc6d7614f4abdc9266eddbc25e3
SideralSkylark <sidikebrahimserage@gmail.com>
2025-05-17 14:53:45 +0200

    added cors config

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"billbridge/database"
 	"billbridge/router"
+
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -14,7 +16,16 @@ func main() {
 	// Initialize router
 	r := router.InitRouter()
 
+	// Configurar CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"}, // ou "*" para todos, se estiveres em dev
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	})
+
 	// Start the server
+	handler := c.Handler(r)
 	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,8 +2,9 @@ module billbridge
 
 go 1.21.6
 
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -13,6 +14,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
